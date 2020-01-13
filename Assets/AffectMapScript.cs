265e237469fba2ca753facf1be69a28771e0dc86
skypekitten9 <victor.enhorning@gmail.com>
2020-01-13 18:52:55 +0100

    Expanded tilemap, changed the type of updates in TileScript() and added AffectMapScript().

---
+++ Assets/AffectMapScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AffectMapScript : MonoBehaviour
+{
+    GameObject tileMap;
+    public bool negative;
+
+    // Update is called once per frame
+    void Update()
+    {
+        tileMap = GameObject.Find("Tilemap");
+        foreach(Transform child in transform)
+        {
+            float distance = Vector3.Distance(child.transform.position, transform.position);
+            child.GetComponent <TileScript>().ApplyIntensity(10f);
+        }
+    }
+}
---
+++ Assets/TileScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    float intensity;
+    Color tileColor;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tileColor = new Color(0, 0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        CalculateColor();
+        GetComponent<Renderer>().material.color = tileColor;
+    }
+
+    private void FixedUpdate()
+    {
+        intensity = 0;
+    }
+
+    void CalculateColor()
+    {
+
+        if(intensity < 0)
+        {
+            tileColor = new Color(1, 1/(intensity*(-1)), 1/(intensity * (-1)), 0);
+        }
+        else
+        {
+            tileColor = new Color(1/intensity, 1/intensity, 1, 0);
+        }
+    }
+
+    public void ApplyIntensity(float distance)
+    {
+        intensity += distance;
+    }
+}
+
